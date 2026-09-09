48ebbe1d40866d0ea162e15cb465f8855c964fe2
Luxho31 <luisse24@hotmail.com>
2026-09-09 13:56:15 -0500

    fix: comment out redundant CORS preflight options handling

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('(.*)', cors(corsOptions))
+// app.options('(.*)', cors(corsOptions))
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/company", companyRoute);
